58e622f255903ecaef42fea47a53c5535d37e5b2
Jordan Pratt <jpratt@mtroyal.ca>
2020-09-03 19:31:41 -0600

    Looking goods.

--- index.js
+++ index.js
@@ -12,36 +12,22 @@ require("dotenv").config();
 // logging is good
 const { createLogger } = require("./src/MyLogger");
 
-// file-base64 lets you encode a file in base64; GitHub API requires
-// content pushed to repos be base64 encoded
-const base64 = require("file-base64");
-
-// adm-zip makes working with zip files (which are what artifacts are
-// downloaded as) reasonably easy
-const admZip = require("adm-zip");
-
 // we need to listen for incoming info from the WebApp
 const http = require("http");
-
-// only use this to handle the request for artifact download;
-// I'm not entirely sure this is needed, since we're already using
-// http...but it makes things easier, so sticking with it for now
-const agent = require("superagent");
 
 // used for some file handling on the server
 const fs = require("fs");
 
-// @octokit/app is a library from GitHub that makes working with GitHub Apps
-// a bit easier, particularly the authentication side
-const { App } = require("@octokit/app");
-
-// @octokit/request is a library from GitHub that makes making GitHub
-// API requests a bit easier
-let { request } = require("@octokit/request");
-
 // smee-client hooks up a locally running node app to a smee.io
 // url (which the GitHub App listens to for events)
 const { startSmeeConnection } = require("./src/SmeeConnection");
+
+let Requester = require("./src/Requester");
+
+const util = require("util");
+let { summaryContentsFrom } = require("./src/SummaryExtractor");
+let writeFile = util.promisify(fs.writeFile);
+let deleteFile = util.promisify(fs.unlink);
 
 /*
 
@@ -52,7 +38,12 @@ The body of the app.
 startSmeeConnection();
 
 const logger = createLogger();
-const app = createApp();
+
+const MY_KEY = fs.readFileSync(
+  ".data/artifactminer.2020-08-26.private-key.pem"
+);
+
+let myRequester = new Requester(process.env.APP_ID, MY_KEY, 11459311);
 
 http.createServer(handleRequest).listen(3333);
 
@@ -61,25 +52,6 @@ http.createServer(handleRequest).listen(3333);
 All the helpers.
 
 */
-
-/**
- * So this "app" is only an object that seems to handle authorization
- * for the GitHub App being used here; don't get me wrong, I'm grateful...
- * but the only place where we use this is when we need to generate a token
- * to do all the requests that are scattered around.
- *
- * Shrug.
- */
-function createApp() {
-  const MY_KEY = fs.readFileSync(
-    ".data/artifactminer.2020-08-26.private-key.pem"
-  );
-
-  return new App({
-    id: process.env.APP_ID,
-    privateKey: MY_KEY,
-  });
-}
 
 /**
  * Look for submission requests and coming in via
@@ -132,102 +104,45 @@ function handleRequest(req, response) {
  */
 async function processSubmissionFrom(payload) {
   const organization = payload.organization.login;
+  const repo = payload.repository.name;
   const workflowRunId = payload.workflow_run.id;
-  const assignmentRepo = payload.repository.name;
-  const installationId = payload.installation.id;
-  const tokenForThisSubmission = await tokenFor(installationId);
 
-  logger.info(`=== incoming submission: ${assignmentRepo}[${organization}]`);
+  logger.info(`=== incoming submission: ${repo}[${organization}]`);
 
-  request = request.defaults({
-    headers: {
-      authorization: `token ${tokenForThisSubmission}`,
-      accept: "application/vnd.github.v3+json",
-    },
-  });
+  const zipRequestResponse = await myRequester
+    .encodedZipFileFor(organization, repo, workflowRunId)
+    .catch((err) => logger.error(err));
 
-  const artifactResponse = await responseToArtifactRequest(
-    organization,
-    assignmentRepo,
-    workflowRunId
-  );
+  const artifact = buildArtifactFrom(zipRequestResponse, organization);
 
-  // logger.info(
-  //   "requested artifact details, received this response: " +
-  //     JSON.stringify(artifactResponse)
-  // );
+  let zipData = zipRequestResponse.data;
 
-  const artifactId = submissionArtifactIdFrom(artifactResponse);
-
-  const downloadResponse = await responseToDownloadRequest(
-    organization,
-    assignmentRepo,
-    artifactId
-  );
-
-  // logger.info(
-  //   "requested download details, received this response: " +
-  //     JSON.stringify(downloadResponse)
-  // );
-
-  const artifact = buildArtifactFrom(downloadResponse, organization);
-
-  // logger.info(
-  //   "built this artifact from the download details: " + JSON.stringify(artifact)
-  // );
-
-  saveToDisk(artifact);
-}
-
-async function responseToArtifactRequest(organization, repo, id) {
-  try {
-    return await request(
-      "GET /repos/{owner}/{repo}/actions/runs/{run_id}/artifacts",
-      {
-        owner: organization,
-        repo: repo,
-        run_id: id,
-        headers: {
-          accept: "application/vnd.github.machine-man-preview+json",
-        },
-      }
-    );
-  } catch (e) {
-    logger.error("could not get an artifact request: " + e);
-    return "Error";
+  if (!zipData) {
+    logger.error("no zippy");
+    return;
   }
-}
 
-async function responseToDownloadRequest(organization, repo, id) {
-  try {
-    return await request(
-      "GET /repos/{owner}/{repo}/actions/artifacts/{artifact_id}/zip",
-      {
-        owner: organization,
-        repo: repo,
-        artifact_id: id,
-      }
-    );
-  } catch (e) {
-    logger.error("attempt to get download failed: " + e);
-    return "Error";
-  }
-}
+  await writeFile(`submissions/${artifact.name}`, Buffer.from(zipData))
+    .then(logger.log("wrote file"))
+    .catch((err) => logger.error(err));
 
-/**
- * You need an installation token to perform all the groovy actions
- * (getting artifact download urls, writing to repositories, and the like)
- *
- * @param {*} payload
- */
-async function tokenFor(installationId) {
-  return app.getInstallationAccessToken({
-    installationId: installationId,
-  });
-}
+  await myRequester
+    .requestZipUpload(zipData, artifact)
+    .then(logger.log("uploaded zip"))
+    .catch((err) => logger.error(err));
 
-function submissionArtifactIdFrom(artifactResponse) {
-  return artifactResponse.data.artifacts[0].id;
+  let summaryContents = await summaryContentsFrom(
+    `submissions/${artifact.name}`
+  ).catch((err) => logger.error(err));
+
+  await myRequester
+    .requestSummaryUpload(summaryContents, artifact)
+    .then(logger.log("uploaded summary"))
+    .catch((err) => logger.error(err));
+
+  await deleteFile(`submissions/${artifact.name}`)
+    .then(logger.info(`=== submission completed for ${artifact.repo}`))
+    .catch((err) => logger.error(err));
 }
 
 function artifactNameFrom(downloadResponse) {
@@ -265,64 +180,4 @@ function buildArtifactFrom(downloadResponse, organization) {
     timestamp: details.timestamp,
     repo: details.repo,
   };
-}
-
-async function saveToDisk(artifact) {
-  const submissionDir = "submissions";
-  const fullPathToZip = `${submissionDir}/${artifact.name}`;
-
-  const writer = fs.createWriteStream(fullPathToZip);
-  writer.on("finish", () => {
-    encodeIt(fullPathToZip, artifact);
-  });
-
-  agent
-    .get(artifact.url)
-    .on("error", function (error) {
-      logger.error("error saving to disk: " + error);
-    })
-    .pipe(writer);
-}
-
-function encodeIt(fullPathToZip, artifact) {
-  base64.encode(fullPathToZip, function (err, base64String) {
-    pushZipToSubmit(base64String, artifact, fullPathToZip);
-  });
-}
-
-function pushZipToSubmit(contents, artifact, fullPathToZip) {
-  request(
-    `PUT /repos/${artifact.organization}/submissions/contents/${artifact.assessment}/${artifact.studentUsername}/${artifact.timestamp}/${artifact.repo}.zip`,
-    {
-      message: "This submit brought to you by Arty the ArtifactMiner Bot.",
-      content: contents,
-    }
-  ).then(() => {
-    pushSummaryToSubmit(fullPathToZip, artifact);
-  });
-}
-
-function pushSummaryToSubmit(fullPathToZip, artifact) {
-  var theZip = new admZip(fullPathToZip);
-  theZip.readFileAsync("reports/summary-report.txt", function (data) {
-    const summaryFileContents = data.toString();
-    request(
-      `PUT /repos/${artifact.organization}/submissions/contents/${artifact.assessment}/${artifact.studentUsername}/${artifact.timestamp}/summary-report.txt`,
-      {
-        message: "This submit brought to you by Arty the ArtifactMiner Bot.",
-        content: Buffer.from(summaryFileContents).toString("base64"),
-      }
-    ).then(deleteArtifactOnServer(artifact, fullPathToZip));
-  });
-}
-
-function deleteArtifactOnServer(artifact, fullPathToZip) {
-  fs.unlink(fullPathToZip, (err) => {
-    if (err) {
-      logger.error(`couldn't delete ${fullPathToZip} on server`);
-      return;
-    } else {
-      logger.info(`=== submission completed for ${artifact.repo}`);
-    }
-  });
 }
